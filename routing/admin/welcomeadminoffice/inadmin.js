68d8d6d0dfef05c44a5970738b6ba5d18eca657e
Gaurav Mehta <gauravmehta1090@gmail.com>
2021-02-06 18:31:01 +0530

    Kuch kuch kra hai

--- routing/admin/welcomeadminoffice/inadmin.js
+++ routing/admin/welcomeadminoffice/inadmin.js
@@ -6,6 +6,10 @@ router.get('/adddoc', (req,res)=>{
     res.render('adddoc');
 })
 
+router.get('/viewdoc', (req,res)=>{
+    res.render('viewdoc');
+})
+
 router.get('/updatedoc', (req,res)=>{
     res.render('updatedoc');
 })
@@ -14,12 +18,20 @@ router.get('/addmed', (req,res)=>{
     res.render('addmed');
 })
 
+router.get('/viewmed', (req,res)=>{
+    res.render('viewmed');
+})
+
 router.get('/updatemed', (req,res)=>{
     res.render('updatemed');
 })
 
 router.get('/addsympt', (req,res)=>{
     res.render('addsympt');
+})
+
+router.get('/viewsympt', (req,res)=>{
+    res.render('viewsympt');
 })
 
 router.get('/updatesympt', (req,res)=>{
