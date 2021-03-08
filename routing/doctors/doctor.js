1e594d2c5575ea13902deca73a5fadf4c146201a
Gaurav Mehta <gauravmehta1090@gmail.com>
2021-03-08 15:27:09 +0530

    Hogye

--- routing/doctors/doctor.js
+++ routing/doctors/doctor.js
@@ -10,23 +10,23 @@ router.post('/viewingdoctors', async (req, res) => {
     };
     console.log(user);
          const alluser=await new Promise((resolve, reject) => {
-             var query = ``;
-             if(user.sortby == 1)
-             {
-                 query = `SELECT * FROM doctors where doctorsname=?`;
-             }
-             else if (user.sortby == 2)
-             {
+            var query = ``;
+            if(user.sortby == 1)
+            {
+                query = `SELECT * FROM doctors ORDER BY doctorsname ASC`;
+            }
+            else if (user.sortby == 2)
+            {
                 query = `SELECT * FROM doctors where doctorsspeciality=?`;
-             }else
-             if (user.sortby == 3)
-              {
-                     query = `SELECT * FROM doctors ORDER BY doctorsfees ASC`;
-             } else
-             if (user.sortby == 4)
-             {
-                    query = `SELECT * FROM doctors `;
-             }    
+            }
+            else if (user.sortby == 3)
+            {
+                query = `SELECT * FROM doctors ORDER BY doctorsfees ASC`;
+            } 
+            else if (user.sortby == 4)
+            {
+                query = `SELECT * FROM doctors `;
+            }    
             connection.query(query,user.prefeby,(err, result) => {
                 if (err) reject(new Error('Something failed (Record Deletion) :' + err));
                 resolve(result);
